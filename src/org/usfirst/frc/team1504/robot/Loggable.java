23b42f517c47586839918d4bdc245f37124f0b7e
Team1504 <Team1504@Team-1504>
2015-10-12 19:45:46 -0400

    Transition to byte conversion within dump methods. More work to do.

--- src/org/usfirst/frc/team1504/robot/Loggable.java
+++ src/org/usfirst/frc/team1504/robot/Loggable.java
@@ -10,33 +10,4 @@ public abstract class Loggable {
 
 	public abstract double[] dump();
 	
-	private byte[] doubleToByte(double d) {
-		byte[] bytes = new byte[8];
-		ByteBuffer.wrap(bytes).putDouble(d);
-		return bytes;
-	}
-	private byte[] longToByte(long l){
-		byte[] bytes = new byte[8];
-		ByteBuffer.wrap(bytes).putLong(l);
-		return bytes;
-	}
-	private byte[] floatToByte(float f) {
-		byte[] bytes = new byte[8];
-		ByteBuffer.wrap(bytes).putFloat(f);
-		return bytes;
-	}
-
-	private byte[] intToByte(int i) {
-		byte[] bytes = new byte[8];
-		ByteBuffer.wrap(bytes).putInt(i);
-		return bytes;
-	}
-
-	private byte[] boolToByte(boolean b) {
-		byte[] bool = { 0 };
-		if (b)
-			bool[0] = 1;
-		return bool;
-	}
-
 }
